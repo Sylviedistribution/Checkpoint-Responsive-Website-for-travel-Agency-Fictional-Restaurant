02d5e4daf2d0ac2236966ad72b2c86fda3e5c26a
Sylvestre <ibombogakossosylvestre@gmail.com>
2025-06-25 00:38:15 +0000

    Update restaurant project content (style, scripts, etc.)

--- Responsive Website Fictional Restaurant/script.js
+++ Responsive Website Fictional Restaurant/script.js
@@ -1,17 +1,26 @@
-window.addEventListener("scroll", function () {
-    const header = document.getElementById("en-tete");
-    if (window.scrollY > window.innerHeight-30) {
-      header.classList.add("scrolled");
-    } else {
-      header.classList.remove("scrolled");
-    }
-  });
+const header = document.getElementById("en-tete");
+const fullHeader = document.querySelector("header");
 
-  window.addEventListener("scroll", function () {
-    const header = document.querySelector("header");
-    if (window.scrollY > window.innerHeight-30) {
-      header.classList.add("scrolled");
-    } else {
-      header.classList.remove("scrolled");
-    }
-  });
+function handleScroll() {
+  if (window.scrollY > window.innerHeight - 30) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+}
+
+function handleResize() {
+  if (window.innerWidth <= 768) {
+    fullHeader.classList.add("scrolled", "position-fixed");
+    fullHeader.classList.remove("position-absolute");
+  } else {
+    fullHeader.classList.remove("scrolled");
+    fullHeader.classList.remove("position-fixed");
+    fullHeader.classList.add("position-absolute");
+  }
+}
+
+// Événements
+window.addEventListener("scroll", handleScroll);
+window.addEventListener("resize", handleResize);
+window.addEventListener("load", handleResize); // pour ajuster au chargement
